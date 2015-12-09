b9120763f9cc2ae74104790e6e4049d8481029a0
Adam Fluke <fluke.adam@gmail.com>
2015-12-09 15:42:52 -0800

    Add associations between teacher, student, and homework through assignment
    -Added two aliased associations to the user, teacher_assignments and
    student_assignments. This is sub-optimal, but expedient right now.
    
    -Further, any teacher can assign any homework based on the assignment
    associations, which could be refactored so only homeworks owned (created)
    by a teacher can be assigned.

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,3 +1,7 @@
 class Homework < ActiveRecord::Base
+  has_many :assignments
+  has_many :students, through: :assignments
+  has_many :teachers, through: :assignments
+
   validates :question, :title, :due, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :role, presence: true
+
+  has_many :teacher_assignments, class_name: 'Assignment', foreign_key: "teacher_id"
+  has_many :student_assignments, class_name: 'Assignment', foreign_key: 'student_id'
 end
