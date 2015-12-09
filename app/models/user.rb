e38be624dd95040477f59b093b031276368cba3c
Adam Fluke <fluke.adam@gmail.com>
2015-12-09 15:50:43 -0800

    Create intial test for assign_homework_to_students

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :teacher_assignments, class_name: 'Assignment', foreign_key: "teacher_id"
   has_many :student_assignments, class_name: 'Assignment', foreign_key: 'student_id'
+
+  def assign_homework_to_students
+    true
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'Teacher' do
+    let(:teacher){ FactoryGirl.create :teacher }
+
+    context '#assign_homework_to_students' do
+      it 'exists' do
+        expect(teacher.assign_homework_to_students).to be_truthy
+      end
+    end
+  end
 end
