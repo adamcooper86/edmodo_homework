a8985c52e1496aa2355bbf53d1e8d3b1b478decc
Adam Fluke <fluke.adam@gmail.com>
2015-12-09 15:10:25 -0800

    Create assignment resource
    -assignments are going to be the join table between students and homeworks
    -Also created spec/factories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529213510) do
+ActiveRecord::Schema.define(version: 20151209225337) do
+
+  create_table "homeworks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "question"
+    t.date     "due"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
