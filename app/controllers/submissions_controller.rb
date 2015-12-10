80a58bf206a3cd6c34af80dfa1ac1a9db3a6112c
Adam Fluke <fluke.adam@gmail.com>
2015-12-10 11:54:56 -0800

    Add submission #create logic to prevent late submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -11,10 +11,16 @@ class SubmissionsController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
     @homework = @assignment.homework
     @submission = Submission.new homework_id: @homework.id, answer: params[:answer]
-    if @submission.save
+    if not_closed && @submission.save
       redirect_to homework_path @homework
     else
-      redirect_to user_path @user
+      flash[:notice] = "Submission not created - passed submission deadline"
+      redirect_to homework_path @homework
     end
+  end
+
+private
+  def not_closed
+    Time.now <= @homework.due
   end
 end
--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -15,9 +15,24 @@ RSpec.describe SubmissionsController, type: :controller do
     it { is_expected.to render_template "new" }
   end
   describe 'POST #create' do
-    subject{ post :create, assignment_id: assignment.id }
+    context 'A not late valide submission' do
+      subject{ post :create, assignment_id: assignment.id }
 
-    it { is_expected.to have_http_status 302 }
-    it { is_expected.to redirect_to homework_path assignment.homework }
+      it { is_expected.to have_http_status 302 }
+      it { is_expected.to redirect_to homework_path assignment.homework }
+    end
+    context 'Trying to post after the submission deadline' do
+      let(:homework){ FactoryGirl.create :homework, due: Date.today - 1 }
+      let(:assignment){ FactoryGirl.create :assignment, homework: homework }
+
+      subject{ post :create, assignment_id: assignment.id }
+
+      it { is_expected.to have_http_status 302 }
+      it { is_expected.to redirect_to homework_path assignment.homework }
+      it 'has a flash message telling them it was not submitted' do
+        subject
+        expect(flash[:notice]).to eq "Submission not created - passed submission deadline"
+      end
+    end
   end
 end
