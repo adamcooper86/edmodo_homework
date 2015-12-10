bff2c1b99f81297ac7e0a4046e138d1d1712267e
Adam Fluke <fluke.adam@gmail.com>
2015-12-09 16:14:06 -0800

    Add teacher instance method to assign homework to a group of students
    -spec tests positive cases. Instances of invalid data cause key errors
    at this point.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,12 @@ class User < ActiveRecord::Base
   has_many :teacher_assignments, class_name: 'Assignment', foreign_key: "teacher_id"
   has_many :student_assignments, class_name: 'Assignment', foreign_key: 'student_id'
 
-  def assign_homework_to_students
-    true
+  def assign_homework_to_students options
+    homework = options.fetch :homework
+    students = options.fetch :students
+
+    students.map do |student|
+      Assignment.create student_id: student.id, teacher_id: self.id, homework_id: homework.id
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Teacher' do
     let(:teacher){ FactoryGirl.create :teacher }
+    let(:student){ FactoryGirl.create :student }
+    let(:homework){ FactoryGirl.create :homework }
 
     context '#assign_homework_to_students' do
-      it 'exists' do
-        expect(teacher.assign_homework_to_students).to be_truthy
+      subject{ teacher.assign_homework_to_students students: [student], homework: homework }
+
+      it 'returns a collection of assignments' do
+        expect(subject[0]).to be_a Assignment
+      end
+
+      it 'should create assignment record' do
+        expect{subject}.to change{Assignment.all.count}.by 1
       end
     end
   end
