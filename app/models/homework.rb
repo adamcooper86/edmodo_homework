1c28f119fcecfdf295df77bcad76a513e6c73685
Adam Fluke <fluke.adam@gmail.com>
2015-12-10 14:22:50 -0800

    Add submissions method to homework

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,8 +1,12 @@
 class Homework < ActiveRecord::Base
   has_many :assignments
-  has_many :submissions
   has_many :students, through: :assignments
   has_many :teachers, through: :assignments
 
   validates :question, :title, :due, presence: true
+
+  def submissions
+    #creates a flat non-nested array of unique submission objects
+    self.assignments.map{|assignment| assignment.submissions }.flatten.uniq
+  end
 end
